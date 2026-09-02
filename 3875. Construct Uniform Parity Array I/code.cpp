aee0e30683ac80bcc7d3040c24d6e25f7bb1ecd2
Bhanu Prabhat <bhanuprabhat95@gmail.com>
2026-09-02 23:17:59 +0530

    3875. Construct Uniform Parity Array I

--- 3875. Construct Uniform Parity Array I/code.cpp
+++ 3875. Construct Uniform Parity Array I/code.cpp
@@ -1,9 +1,11 @@
-#include <bits/stdc++.h>.
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool uniformArray(vector<int>& nums1) {
+    bool uniformArray(vector<int> &nums1)
+    {
         // There is no case in which we can't create a nums2 array in which all numbers are odd or even.
         return true;
     }
