d1cdea14a3dd1f6373d501766d7533cf447dabff
Bhanu Prabhat <bhanuprabhat95@gmail.com>
2026-09-04 11:30:06 +0530

    3904. Smallest Stable Index II

--- 3904. Smallest Stable Index II/code.cpp
+++ 3904. Smallest Stable Index II/code.cpp
@@ -1,25 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int firstStableIndex(vector<int>& nums, int k) {
+    int firstStableIndex(vector<int> &nums, int k)
+    {
         int n = nums.size();
         vector<int> m(n);
         int leftMax = INT_MIN;
         int rightMin = INT_MAX;
-        
+
         // storing every index's right minimun value
-        for(int i = n - 1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--)
+        {
             rightMin = min(rightMin, nums[i]);
             m[i] = rightMin;
         }
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             leftMax = max(leftMax, nums[i]);
 
             // if we found index which is stable return it, because we have to return minimum index
-            if(leftMax - m[i] <= k) return i;
+            if (leftMax - m[i] <= k)
+                return i;
         }
 
         return -1;
